3ac6a408ae16006b1228dac064955b70b5bda52b
afiatamanna06 <afiatamanna06@gmail.com>
2021-02-27 00:37:34 +0600

    info.html updated

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function searchBengali() {
     var searchInput = document.getElementById('search-meaning');
     var word = searchInput.value.toLowerCase();
-    var output = document.getElementById('');
+    var output = document.getElementById('meaning');
 }
