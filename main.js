4ec1684366841fd5aaf687d50a342e036f7fbd88
afiatamanna06 <afiatamanna06@gmail.com>
2021-02-27 21:12:04 +0600

    hashing class updated

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const databaseURL = 'https://raw.githubusercontent.com/afiatamanna06/EnglishtoBanglaDictionary/main/database/E2Bdatabase.json';
 const PRIME = 921023456789;
+const RADIX = 256;
 class Dictionary {
     database
     numberOfWords
@@ -33,6 +34,24 @@ class Hashing {
         } else {
             a = this.primaryHashA;
             b = this.primaryHashB;
+        }
+        /*  A 7 characters Unicode word may map to approximately 10^17 (256^7) size integer. In JS, Number
+            type has max limit of a decimal number of approximately 16 digits.
+            Thus a word cannot be mapped to number more than that. So taking
+            the remainder of a prime which is of 12 digits in decimal, so 
+            that the multiplication val*RADIX never crosses a number that is 
+            more than 16 digits.
+
+            One complication of this is that there might be cases where two 'different'
+            words mapping to the 'same' numerical key value even though theoretically
+            every (a, b) should have generated a unique key pair (r, s). We can show
+            that this possibility is very very low since in practice total number of words
+            is only around ~17000 and the chance that two different words map to the
+            same integer k (mod PRIME) is 17000/PRIME = 17000/921023456789 ~ 1.8e-6%
+        */
+
+        for (var i = 0; i < word.length; i++) {
+            val = ((val * RADIX) % PRIME + word.charCodeAt(i)) % PRIME;
         }
     }
 }
