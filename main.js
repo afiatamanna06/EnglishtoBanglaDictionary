89489beab039793595e77b0ef81f1b53627bc143
afiatamanna06 <afiatamanna06@gmail.com>
2021-02-27 10:27:23 +0600

    hashing class added

--- main.js
+++ main.js
@@ -3,7 +3,22 @@ const databaseURL = 'https://raw.githubusercontent.com/afiatamanna06/EnglishtoBa
 class Dictionary {
     database
     numberOfWords
-};
+}
+
+class Hashing {
+    hashTable;
+    hashTableKeys;
+    primaryHashA = null;
+    primaryHashB = null;
+
+    initializeHashTable() {
+        this.hashTable = new Array(dictionary.numberOfWords);
+        for (var i = 0; i < dictionary.numberOfWords; i++) {
+            this.hashTable[i] = [];
+        }
+        this.hashTableKeys = new Array(dictionary.numberOfWords).fill(null);
+    }
+}
 
 function searchBengali() {
     var searchInput = document.getElementById('search-meaning');
