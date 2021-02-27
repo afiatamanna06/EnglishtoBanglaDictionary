0d93dd002fabd5cbc2e29e7e816a321d890f9f01
afiatamanna06 <afiatamanna06@gmail.com>
2021-02-27 10:19:54 +0600

    dictionary class added

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const databaseURL = 'https://raw.githubusercontent.com/afiatamanna06/EnglishtoBanglaDictionary/main/database/E2Bdatabase.json';
 
+class Dictionary {
+    database
+    numberOfWords
+};
+
 function searchBengali() {
     var searchInput = document.getElementById('search-meaning');
     var word = searchInput.value.toLowerCase();
